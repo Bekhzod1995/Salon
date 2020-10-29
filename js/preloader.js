4a5655e4224c22ee7081fb48c20c83d18d77e6ea
Bekhzod1995 <81156beka@gmail.com>
2020-10-29 17:54:09 +0500

    adding for the first time

---
+++ js/preloader.js
@@ -0,0 +1,6 @@
+var hellopreloader = document.getElementById("preloader");
+function fadeOutnojquery(el) {
+	el.style.opacity = 1;
+	var interhellopreloader = setInterval(function () { el.style.opacity = el.style.opacity - 0.05; if (el.style.opacity <= 0.05) { clearInterval(interhellopreloader); hellopreloader.style.display = "none"; } }, 16);
+}
+window.onload = function () { setTimeout(function () { fadeOutnojquery(hellopreloader); }, 1000); }
---
+++ js/script.js
@@ -0,0 +1,50 @@
+	AOS.init();
+	const  mybutton = document.getElementById("myBtn");
+	window.onscroll = function() {scrollFunction()};
+
+	function scrollFunction() {
+		if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+		mybutton.style.display = "block";
+		} else {
+		mybutton.style.display = "none";
+		}
+	}
+
+	function topFunction() {
+		document.body.scrollTop = 0;
+		document.documentElement.scrollTop = 0;
+	}
+
+	$('.autoplay').slick({
+		slidesToShow: 3,
+			slidesToScroll: 1,
+			autoplay: true,
+			autoplaySpeed: 2000,
+			arrows: true,
+			adoptiveHeight: true,
+			autoplay: true,
+			autoplaySpeed: 2000,
+			centerMode: true,
+			centerPadding: '60px',
+			slidesToShow: 3,
+			responsive: [
+				{
+		breakpoint: 768,
+					settings: {
+		arrows: false,
+						centerMode: true,
+						centerPadding: '40px',
+						slidesToShow: 3
+					}
+				},
+				{
+		breakpoint: 480,
+					settings: {
+		arrows: false,
+						centerMode: true,
+						centerPadding: '40px',
+						slidesToShow: 1
+					}
+				}
+			]
+		});
