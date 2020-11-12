b5f5801ebeb20b5cfa2e217b4ce0f352f7dcd2a2
Bekhzod1995 <81156beka@gmail.com>
2020-11-13 00:39:31 +0500

    changed prealoder

--- js/script.js
+++ js/script.js
@@ -37,3 +37,55 @@ $('.autoplay').slick({
 		}
 	]
 });
+
+// function matchMedia(width) {
+// 	return window.matchMedia(`(max-width: ${width}px)`);
+// }
+
+
+// const video1 = document.getElementById('video1');
+// console.log('scrollWidth: ', window.scrollWidth);
+// if (matchMedia(1200)) {
+// 	console.log('we are 1200')
+// 	video1.style.width = '900px';
+// 	video1.style.height = '450px';
+// 	video1.style.border = '5px solid red';
+// }
+// if (matchMedia(900)) {
+// 	console.log('we are 900')
+// 	video1.style.width = '600px';
+// 	video1.style.height = '350px';
+// 	video1.style.border = '5px solid green';;
+// }
+// if (matchMedia(600)) {
+// 	console.log('we are 600')
+// 	video1.style.width = '300px';
+// 	video1.style.height = '250px';
+// 	video1.style.border = '5px solid blue';
+// }
+
+// (
+
+// 	function videpAdopter() {
+// 		const video1 = document.getElementById('video1');
+// 		console.log('scrollWidth: ', window.scrollWidth);
+// 		if (matchMedia(1200)) {
+// 			console.log('we are 1200')
+// 			video1.style.width = '900px';
+// 			video1.style.height = '450px';
+// 			video1.style.border = '5px solid red';
+// 		}
+// 		if (matchMedia(900)) {
+// 			console.log('we are 900')
+// 			video1.style.width = '600px';
+// 			video1.style.height = '350px';
+// 			video1.style.border = '5px solid green';;
+// 		}
+// 		if (matchMedia(600)) {
+// 			console.log('we are 600')
+// 			video1.style.width = '300px';
+// 			video1.style.height = '250px';
+// 			video1.style.border = '5px solid blue';
+// 		}
+// 	}
+// )();
