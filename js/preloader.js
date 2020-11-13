f4cfb77f272d217b11c35dbcc57163a8eda11ed1
Bekhzod1995 <81156beka@gmail.com>
2020-11-13 09:49:04 +0500

    fixed adoptivity for mobile preloader

--- js/preloader.js
+++ js/preloader.js
@@ -1,8 +1,8 @@
-var hellopreloader = document.getElementById("test");
-// var hellopreloader = document.getElementById("preloader");
+// var hellopreloader = document.getElementById("test");
+// // var hellopreloader = document.getElementById("preloader");
 
-function fadeOutnojquery(el) {
-	el.style.opacity = 1;
-	var interhellopreloader = setInterval(function () { el.style.opacity = el.style.opacity - 0.05; if (el.style.opacity <= 0.05) { clearInterval(interhellopreloader); hellopreloader.style.display = "none"; } }, 16);
-}
-window.onload = function () { setTimeout(function () { fadeOutnojquery(hellopreloader); }, 1000); }
+// function fadeOutnojquery(el) {
+// 	el.style.opacity = 1;
+// 	var interhellopreloader = setInterval(function () { el.style.opacity = el.style.opacity - 0.05; if (el.style.opacity <= 0.05) { clearInterval(interhellopreloader); hellopreloader.style.display = "none"; } }, 16);
+// }
+// window.onload = function () { setTimeout(function () { fadeOutnojquery(hellopreloader); }, 1000); }
